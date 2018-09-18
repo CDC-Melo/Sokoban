5f483d1845d8649abfce837333d318332becb9a7
CDC-Melo <43125330+CDC-Melo@users.noreply.github.com>
2018-09-18 22:05:22 +0800

    h
    ftss

---
+++ file/Player.h
@@ -0,0 +1,37 @@
+#pragma once
+#include<QGraphicsScene>
+#include<QObject>
+#include<QGraphicsRectItem>
+#include<QKeyEvent>
+#include<QBrush>
+#include<QPen>
+#include<QList>
+//#include<Boxes.h>
+#include<QMainWindow>
+//
+#include"block.h"
+#include"boxes.h"
+#include"spot.h"
+#include"parameter.h"
+class Player :public QObject, public QGraphicsRectItem
+{
+	Q_OBJECT
+public:
+	Player();
+	void keyPressEvent(QKeyEvent * event);
+	void arrival(Boxes * box);
+	void up();
+	void down();
+	void left();
+	void right();
+	/*void left_pull();
+	void right_pull();
+	void up_pull();
+	void down_pull();*/
+	~Player();
+
+signals:
+	void countbox();
+
+
+};
